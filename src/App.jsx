b0b85cfeea9a6436464b3fe97e68a9aab7a5f795
symbioids <parasshresthanever@gmail.com>
2024-04-18 18:21:08 +0545

    appwrite setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  return (
+    <>
+     <h1>A blog app | appwrite</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
+
+    appwriteUrl: String(import.meta.env.VITE_APP_PROJECT_ID),
+    appwriteUrl: String(import.meta.env.VITE_APP_COLLECTION_ID),
+    appwriteUrl: String(import.meta.env.VITE_APP_DATABASE_ID),
+    appwriteUrl: String(import.meta.env.VITE_APP_BUCKET_ID),}
+    export default conf
